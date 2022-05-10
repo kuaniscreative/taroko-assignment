6802d37df981955bcc6e1fa0a67dd21573140249
kuaniscreative <benben19911020@gmail.com>
2022-05-10 20:53:12 +0800

    feat(contact) add CRUD api

---
+++ core/contact/api/contacts.ts
@@ -0,0 +1,115 @@
+export async function getContacts() {
+  return fetch('/api/contacts')
+    .then((res) => {
+      return res.json();
+    })
+}
+
+export interface GetContactParams {
+  id: number;
+}
+
+export async function getContact(params: GetContactParams) {
+  const {
+    id
+  } = params;
+
+  console.log(id, 'params');
+  
+
+  return fetch(`/api/contacts/${id}`)
+    .then((res) => {
+      return res.json();
+    })
+}
+
+export interface CreateContactParams {
+  firstName?: string;
+  lastName?: string;
+  job?: string;
+  description?: string;
+}
+
+export async function createContact(params: CreateContactParams) {
+  const {
+    firstName,
+    lastName,
+    job,
+    description,
+  } = params;
+
+  const body = JSON.stringify({
+    contact: {
+      "first_name": firstName,
+      "last_name": lastName,
+      job,
+      description
+    }
+  })
+
+  return fetch('/api/contacts', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body,
+  })
+    .then((res) => {
+      return res.json();
+    })
+}
+
+export interface UpdateContactParams {
+  id: number;
+  firstName?: string;
+  lastName?: string;
+  job?: string;
+  description?: string;
+}
+
+export async function updateContact(params: UpdateContactParams) {
+  const {
+    id,
+    firstName,
+    lastName,
+    job,
+    description,
+  } = params;
+
+  const body = JSON.stringify({
+    contact: {
+      "first_name": firstName,
+      "last_name": lastName,
+      job,
+      description
+    }
+  })
+
+  return fetch(`/api/contacts/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body,
+  })
+    .then((res) => {
+      return res.json();
+    })
+}
+
+export interface DeleteContactParams {
+  id: number;
+}
+
+export async function deleteContact(params: DeleteContactParams) {
+  const {
+    id,
+  } = params;
+
+  return fetch(`/api/contacts/${id}`, {
+    method: 'DELETE'
+  })
+    .then((res) => {
+      return res.json();
+    })
+}
