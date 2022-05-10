c592a1f35e48dbe695207fae63766f745fa91e14
kuaniscreative <benben19911020@gmail.com>
2022-05-10 21:48:38 +0800

    feat(contact-list) add className prop

--- core/contact/components/ContactList/ContactList.tsx
+++ core/contact/components/ContactList/ContactList.tsx
@@ -1,17 +1,23 @@
+import clsx from "clsx";
 import { ReactNode } from "react";
 import classes from './contact-list.module.scss';
 
 export interface ContactListProps {
   children?: ReactNode;
+  className?: string;
 }
 
 function ContactList(props: ContactListProps) {
   const {
-    children
+    children,
+    className
   } = props;
 
   return (
-    <div className={classes.root}>
+    <div className={clsx(
+      classes.root,
+      className
+    )}>
       {children}
     </div>
   )
