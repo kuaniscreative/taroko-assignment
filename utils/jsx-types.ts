0cd6b7476c5a782068969d7450bd974ed9d4efc3
kuaniscreative <benben19911020@gmail.com>
2022-05-10 14:14:44 +0800

    feat(utils) add jsx-types

---
+++ utils/jsx-types.ts
@@ -0,0 +1,19 @@
+import {
+  ComponentPropsWithoutRef,
+  DetailedHTMLProps,
+  ElementType,
+  SVGProps,
+} from 'react';
+
+export type NativeElementTag = keyof JSX.IntrinsicElements;
+export type NativeElementPropsWithoutKeyAndRef<T extends NativeElementTag> =
+  Omit<JSX.IntrinsicElements[T], 'key' | 'ref'>;
+export type NativeElement<T extends NativeElementTag> =
+  JSX.IntrinsicElements[T] extends DetailedHTMLProps<infer _A, infer E>
+    ? E
+    : JSX.IntrinsicElements[T] extends SVGProps<infer E>
+      ? E
+      : unknown;
+
+export type ComponentPropsWithoutKeyAndRef<T extends ElementType> = Omit<ComponentPropsWithoutRef<T>, 'key'>;
+
