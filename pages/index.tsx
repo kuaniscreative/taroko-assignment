197284fa4a50fa2b74624c4b81bb6bc585422cfb
kuaniscreative <benben19911020@gmail.com>
2022-05-10 21:26:30 +0800

    feat(home) apply contact list and fetched data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,43 @@
+import ContactCard from "@/core/contact/components/ContactCard/ContactCard";
+import ContactList from "@/core/contact/components/ContactList/ContactList";
+import { useQuery } from 'react-query';
+import { getContacts } from "../core/contact/api/contacts";
+import { Contact } from "@/core/contact/typing";
+import { CONTACTS_QUERY_KEY } from "@/core/contact/constant";
+import classes from './index.module.scss';
+
+
 export default function Home() {
+  // fetch contacts
+  const { 
+    data
+  } = useQuery<QueryResult<Contact[]>>(CONTACTS_QUERY_KEY, getContacts);
+  const {
+    data: contacts
+  } = data || {};
+
+  console.log(contacts, 'contacts');
+  
+  
   return (
-    <div>
-      welcome to taroko assignment
+    <div className={classes.root}>
+      <ContactList>
+        {contacts?.map(({
+          id,
+          first_name,
+          last_name,
+          job,
+          description,
+        }) => (
+          <ContactCard 
+            key={id}
+            firstName={first_name}
+            lastName={last_name}
+            job={job}
+            description={description}
+          />
+        ))}
+      </ContactList>
     </div>
   )
 }
